631a2694bcb5621a4f4da5a1cfb783ec46de563f
Vinicius dos Reis de Jesus <vinicius.jesus@amo.delivery>
2025-03-19 12:47:56 -0300

    refactor: add schema to update member

--- src/interfaces/http/Membro/MembroSchema.js
+++ src/interfaces/http/Membro/MembroSchema.js
@@ -25,6 +25,24 @@ module.exports = {
     esportes: Joi.array().items(Joi.string().required()).optional(),
     games: Joi.array().items(Joi.string().required()).optional()
   }),
+  putMembroSchema: Joi.object().keys({
+    inscricao: Joi.string().trim().optional(),
+    nome: Joi.string().trim().required().min(4).max(200),
+    matricula: Joi.string().trim().required(),
+    cpf: Joi.string().trim().regex(/[0-9]+/).min(11).max(14).required().allow(null).default(null),
+    email: Joi.string().trim().email().lowercase().required(),
+    endereco: Joi.object().keys({
+      cep: Joi.string().trim().optional(),
+      rua: Joi.string().trim().required(),
+      numero: Joi.string().required().allow(null).default(null),
+      bairro: Joi.string().trim().required(),
+      complemento: Joi.string().trim().optional(),
+      cidade: Joi.string().trim().required()
+    }).required(),
+    telefone: Joi.string().trim().required(),
+    telefone_emergencia: Joi.string().trim().optional(),
+    condicao: Joi.string().optional().allow(null),
+  }),
   getAllMembrosSchema: Joi.object().keys({
     page: Joi.number().optional().default(1),
     limit: Joi.number().optional().default(25)
